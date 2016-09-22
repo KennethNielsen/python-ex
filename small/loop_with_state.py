9093eec762a06aee310a9cd6bbf4a3d9a95478e5
Kenneth Nielsen <k.nielsen81@gmail.com>
2016-09-22 07:24:29 +0200

    Added a small exercise more

---
+++ small/loop_with_state.py
@@ -0,0 +1,42 @@
+
+"""Exercise
+
+The words in the words list has some sections in it, starte and ended
+by the words 'start' and 'end'. Furthermore, there is also the
+'content_start' marker, that indicates where the content we are
+interistedin starts.
+
+The exercise is to print out the words in the even numbered sections
+after content start.
+
+"""
+
+words = [
+    'start',
+    'no',
+    'end',
+    'content_start',
+    'start',
+    'complete',
+    'nonsense',
+    'end',
+    'start',
+    'Python',
+    'has',
+    'end',
+    'start',
+    'even',
+    'more',
+    'rubbish',
+    'no',
+    'end',
+    'start',
+    'an',
+    'easy',
+    'expressive',
+    'syntax!',
+    'end',
+]
+
+
+# ... your solution here
